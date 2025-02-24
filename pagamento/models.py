3557ad85a57e566318db025d6a6ecb774569a7df
Bohredd <dapereira@inf.ufsm.br>
2025-02-24 19:42:40 -0300

    feat: pagamento + planos start de api ipara mercado pago

--- pagamento/models.py
+++ pagamento/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Pagamento(models.Model):
 
@@ -7,7 +8,9 @@ class Pagamento(models.Model):
     status = models.CharField(max_length=255)
     tipo = models.CharField(max_length=255)
     plano = models.ForeignKey('plano.Plano', on_delete=models.CASCADE)
-    empresa = models.ForeignKey('empresa.Empresa', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hash_mercadopago = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.valor
--- plano/models.py
+++ plano/models.py
@@ -3,12 +3,11 @@ from django.db import models
 class Plano(models.Model):
 
     nome = models.CharField(max_length=255)
-    descricao = models.TextField()
+    descricao = models.TextField(null=True, blank=True)
     valor = models.FloatField()
-    duracao = models.IntegerField()
-    quantidade_servicos = models.IntegerField()
+    duracao_em_dias = models.IntegerField()
+    quantidade_empresas = models.IntegerField()
     quantidade_funcionarios = models.IntegerField()
-    quantidade_clientes = models.IntegerField()
 
     def __str__(self):
         return self.nome
