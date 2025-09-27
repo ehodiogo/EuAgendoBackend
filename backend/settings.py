59ca65eed6658d17c83597905678971c9657546c
Diogo <dapereira@inf.ufsm.br>
2025-09-26 23:37:15 -0300

    feat: dockerização para projeto ir p producao

--- backend/settings.py
+++ backend/settings.py
@@ -45,10 +45,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:8080",
     "https://localhost:8080",
-    "https://backend-production-7438.up.railway.app",
-    "http://backend-production-7438.up.railway.app",
-    "http://frontend-production-8641.up.railway.app",
-    "https://frontend-production-8641.up.railway.app",
+    "https://backend-production-6587.up.railway.app",
+    "http://backend-production-6587.up.railway.app",
+    "http://frontend-production-9e87.up.railway.app",
+    "https://frontend-production-9e87.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
