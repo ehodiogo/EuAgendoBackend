0ae65d6065d8ecd1013995c917b917a75fac34c0
Diogo <dapereira@inf.ufsm.br>
2025-10-02 10:00:24 -0300

    feat: email unique no usuario agora

--- api/models.py
+++ api/models.py
@@ -2,3 +2,4 @@ from django.db import models
 from django.contrib.auth.models import User
 
 User.add_to_class('empresas', models.ManyToManyField('empresa.Empresa', related_name='users'))
+User._meta.get_field('email')._unique = True
