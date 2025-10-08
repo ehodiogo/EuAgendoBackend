0abaf387caad136e27fe26fb38919c07ede0a726
Diogo <dapereira@inf.ufsm.br>
2025-10-08 14:28:41 -0300

    hotfix: config is not defined

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
+from config import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
